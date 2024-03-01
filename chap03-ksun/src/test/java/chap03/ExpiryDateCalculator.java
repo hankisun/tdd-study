30836e55ec830f55d96182ff2dff00c0bfa4cc5c
kisun.han <kisun.han@gmail.com>
2024-03-02 06:28:57 +0900

    ch3_pay_10k_won2

--- chap03-ksun/src/test/java/chap03/ExpiryDateCalculator.java
+++ chap03-ksun/src/test/java/chap03/ExpiryDateCalculator.java
@@ -5,6 +5,7 @@ import java.time.YearMonth;
 
 public class ExpiryDateCalculator {
     LocalDate calculateExpiryDate(LocalDate billingDate, int payAmount){
-        return LocalDate.of(2024, 4, 1);
+        return billingDate.plusMonths(1);
+
     }
 }
