ba938abb070f1f0246dd81a0e62c81b4e58be970
kisun.han <kisun.han@gmail.com>
2024-03-02 07:14:16 +0900

    ch3_code_clean

--- chap03-ksun/src/test/java/chap03/ExpiryDateCalculatorTest.java
+++ chap03-ksun/src/test/java/chap03/ExpiryDateCalculatorTest.java
@@ -9,20 +9,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ExpiryDateCalculatorTest {
     @Test
     void pay_10K_won(){
-        LocalDate billingDate = LocalDate.of(2024,3,1);
-        int payAmount = 10_000;
+        assertExpiryDate(
+                LocalDate.of(2024, 3, 1), 10_000, 
+                LocalDate.of(2024, 4, 1));
+        assertExpiryDate(
+                LocalDate.of(2024, 5, 5), 10_000, 
+                LocalDate.of(2024, 6, 5));
+    }
 
+    private void assertExpiryDate(
+        LocalDate billingDate, int payAmount, LocalDate expectedExpiryDate
+    )  {
         ExpiryDateCalculator cal = new ExpiryDateCalculator();
-        LocalDate expiryDate = cal.calculateExpiryDate(billingDate, payAmount);
-
-        assertEquals(LocalDate.of(2024, 4, 1), expiryDate);
-
-        LocalDate billingDate2 = LocalDate.of(2024,5,5);
-        int payAmount2 = 10_000;
-
-        ExpiryDateCalculator cal2 = new ExpiryDateCalculator();
-        LocalDate expiryDate2 = cal2.calculateExpiryDate(billingDate2, payAmount2);
-
-        assertEquals(LocalDate.of(2024, 6, 5), expiryDate2);
+        LocalDate realExpiryDate = cal.calculateExpiryDate(billingDate, payAmount);
+        assertEquals(expectedExpiryDate, realExpiryDate);
     }
 }
