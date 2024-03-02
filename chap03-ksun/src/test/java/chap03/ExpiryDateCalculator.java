e5c63d42fa9e31de8051701547b618d65c834fcd
kisun.han <kisun.han@gmail.com>
2024-03-02 14:54:10 +0900

    ch3_code_clean

--- chap03-ksun/src/test/java/chap03/ExpiryDateCalculator.java
+++ chap03-ksun/src/test/java/chap03/ExpiryDateCalculator.java
@@ -7,17 +7,32 @@ public class ExpiryDateCalculator {
     LocalDate calculateExpiryDate(PayData payData) {
         int addMonths = payData.getPayAmount() / 10_000;
         if(payData.getFirstBillingDate() != null) {
-            LocalDate candidateExp = payData.getBillingDate().plusMonths(addMonths);
-            if(payData.getFirstBillingDate().getDayOfMonth() != candidateExp.getDayOfMonth()) {
-                if(YearMonth.from(candidateExp).lengthOfMonth() < 
-                        payData.getFirstBillingDate().getDayOfMonth()) {
-                    return candidateExp.withDayOfMonth(
-                        YearMonth.from(candidateExp).lengthOfMonth());
-                }
-                return candidateExp.withDayOfMonth(
-                        payData.getFirstBillingDate().getDayOfMonth());
-            }
+            return expiryDateUsingFirstBillingDate(payData, addMonths);
+        } else {
+            return payData.getBillingDate().plusMonths(addMonths);
         }
-        return payData.getBillingDate().plusMonths(addMonths);
+    }
+
+    private LocalDate expiryDateUsingFirstBillingDate(
+            PayData payData, int addMonths) {
+        LocalDate candidateExp = payData.getBillingDate().plusMonths(addMonths);
+        if(isNotSameDayOfMonth(payData.getFirstBillingDate(), candidateExp)) {
+            final int dayLenOfCandiMon = lastDayOfMonth(candidateExp);
+            final int dayOfFirstBilling = payData.getFirstBillingDate().getDayOfMonth();
+            if(dayLenOfCandiMon < dayOfFirstBilling) {
+                return candidateExp.withDayOfMonth(dayLenOfCandiMon);
+            }
+            return candidateExp.withDayOfMonth(dayOfFirstBilling);
+        } else {
+            return candidateExp;
+        }
+    }
+
+    private boolean isNotSameDayOfMonth(LocalDate date1, LocalDate date2) {
+        return date1.getDayOfMonth() != date2.getDayOfMonth();
+    }
+
+    private int lastDayOfMonth(LocalDate date) {
+        return YearMonth.from(date).lengthOfMonth();
     }
 }
